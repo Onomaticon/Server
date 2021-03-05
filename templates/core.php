e872ab8251b20a70237e35ca9e946bd2d811d42e
Ed Baker <edwbaker@gmail.com>
2021-03-05 12:27:25 +0000

    Meta tags in head.cccccc

--- templates/core.php
+++ templates/core.php
@@ -1,7 +1,10 @@
-<html>
+<html lang="<?php print t($config["default_lang"]); ?>">
 <head>
 <meta charset = "UTF-8">
 <title><?php print t($config["site_name"]); ?></title>
+<meta name="Generator" content="Onomaticon (https://onomaticon.github.io/)"/>
+<meta name="author" content="<?php print($config["author"]); ?>">
+<meta name="description" content="<?php print($config["description"]); ?>">
 <link rel="stylesheet" type="text/css" href="css/default.css" />
 </head>
 
