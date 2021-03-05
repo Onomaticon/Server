e8be2df1ad94f952c5e224bc8ae4d9a7305c34ad
Ed Baker <edwbaker@gmail.com>
2021-03-05 12:54:10 +0000

    Update core.php

--- templates/core.php
+++ templates/core.php
@@ -2,7 +2,7 @@
 <head>
 <meta charset = "UTF-8">
 <title><?php print t($config["site_name"]); ?></title>
-<meta name="Generator" content="Onomaticon (https://onomaticon.github.io/)"/>
+<meta name="Generator" content="Ontomasticon (https://ontomasticon.github.io/)"/>
 <meta name="author" content="<?php print($config["author"]); ?>">
 <meta name="description" content="<?php print($config["description"]); ?>">
 <link rel="stylesheet" type="text/css" href="css/default.css" />
