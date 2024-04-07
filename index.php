789ef157e85adcea93d7902138d3b98a1dfc7e8a
Ed Baker <edwbaker@gmail.com>
2024-04-07 22:49:49 +0000

    Error printing.

--- index.php
+++ index.php
@@ -1,8 +1,4 @@
 <?php
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-
 // Ontomasticon: a simple, lightweight, PHP-based ontology browser.
 // Department of Information Retrieval
 
